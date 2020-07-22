7db9b2abd6fbd72aa0ba73809f5c28b985595976
Huy Nguyen <huypn82@gmail.com>
2020-07-22 11:29:21 +0200

    Added more comments

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+// Defining id elements
 const daysElement = document.getElementById("days");
 const hoursElement = document.getElementById("hours");
 const minutesElement = document.getElementById("minutes");
 const secondsElement = document.getElementById("seconds");
 
+// Function to get time difference between target date and current date
 const getTimeDifference = (targetDate) => {
     let diff = targetDate.getTime() - Date.now();
 
+    // When count down is finished
     if (diff < 0) {
         return {
             days: 0,
@@ -15,6 +18,7 @@ const getTimeDifference = (targetDate) => {
         };
     }
 
+    // Convert to miliseconds
     const days = Math.floor(diff / (1000 * 60 * 60 * 24));
     const hours = Math.floor(diff / (1000 * 60 * 60) % 24);
     const minutes = Math.floor(diff / (1000 * 60) % 60);
@@ -23,6 +27,7 @@ const getTimeDifference = (targetDate) => {
     return { days, hours, minutes, seconds };
 };
 
+// Update and display date and time
 const updateTime = (targetDate) => {
     const { days, hours, minutes, seconds } = getTimeDifference(targetDate);
     daysElement.innerText = days;
@@ -31,6 +36,7 @@ const updateTime = (targetDate) => {
     secondsElement.innerText = seconds.toString().padStart(2, "0");
 };
 
+// Set target date and update every second
 const targetDate = new Date("2020-08-01 09:00:00");
 updateTime(targetDate);
 setInterval(updateTime, 1000, targetDate);
